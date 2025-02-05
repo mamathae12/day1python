ed29745da9e34147fb5b83e51aa3c081a17d4144
mamathae12 <161188340+mamathae12@users.noreply.github.com>
2025-02-05 11:37:59 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,68 @@
+#printing single number
+print(3)
+#printing multiple numbers
+print("2,3,4,5,6")
+#With out variables 
+print(2-6)
+print(3*6)
+print(2/4)
+print(3**4)
+print(3+9)
+print(3%6)
+print(2//5)
+#integer and float
+print(type(2))
+print(type(3.6))
+print(type(2.0))
+#using variables 
+num1 = 23456789
+num2 = 25789999
+print(num1 +num2)
+print(num1 -num2)
+print(num1 /num2)
+print(num1 *num2)
+
+print("Hai Siri")
+iphone = "Hai Siri"
+print(iphone)
+print(type(iphone))
+print(len(iphone))
+num1 = "      "
+print(len(num1))
+#indexing
+str1 = 'Mammu Siri'
+print(str1[-3])
+print(str1[-9])
+print(len(str1))
+print(str1[-10])
+#slicing
+print(str1[0:21])
+print(str1[3:9])
+print(str1[1:8])
+#slicing with negative integers
+print(str1[-13:-3])
+print("check",str1[-3:-9:-1])
+print(str1[-8:-12:-1])
+print(str1[0:16:2])
+print(str1[0:16:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
